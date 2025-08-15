3b1b2707766ecba607af22a15f67477bc0817571
SonOfSofaman <sonofsofaman@gmail.com>
2025-08-15 05:43:20 -0500

    integrate with interrupt and data-in buttons

--- Uniqromascope.ino
+++ Uniqromascope.ino
@@ -1,25 +1,56 @@
 volatile int counter = 0;
+volatile bool takeSample = false;
+volatile unsigned long data = 0UL;
 
 void setup()
 {
-	Serial.begin(9600);
+	Serial.begin(115200);
 	pinMode(LED_BUILTIN, OUTPUT);
-	pinMode(2, INPUT_PULLUP);
-	attachInterrupt(digitalPinToInterrupt(2), incrementCounter, FALLING);
+	pinMode(2, INPUT);
+	pinMode(4, INPUT);
+	attachInterrupt(digitalPinToInterrupt(2), interruptHandler, RISING);
 }
 
 void loop()
 {
-	Serial.print("foo 0002 ... ");
-	Serial.print("counter:");
-	Serial.println(counter);
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(5000);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(100);
+	// digitalWrite(LED_BUILTIN, HIGH);
+	// delay(5000);
+	// digitalWrite(LED_BUILTIN, LOW);
+	// delay(100);
+
+	if (takeSample)
+	{
+		bool bit = digitalRead(4);
+
+		data = data << 1;
+		data |= bit;
+
+		Serial.print("foo 0003 ... ");
+		Serial.print("counter:");
+		Serial.print(counter);
+
+		byte r = (data & 0x000000FFUL) >> 0;
+		byte g = (data & 0x0000FF00UL) >> 8;
+		byte b = (data & 0x00FF0000UL) >> 16;
+
+		Serial.print(" b,g,r:");
+		Serial.print(b);
+		Serial.print(",");
+		Serial.print(g);
+		Serial.print(",");
+		Serial.print(r);
+
+		Serial.println();
+
+		takeSample = false;
+	}
 }
 
-void incrementCounter()
+void interruptHandler()
 {
 	counter++;
+	if (takeSample == false)
+	{
+		takeSample = true;
+	}
 }
